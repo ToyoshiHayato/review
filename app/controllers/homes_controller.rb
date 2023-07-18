b3d3d89c314fadf67a56ed991ba423f3a57e521d
EC2 Default User <ec2-user@ip-172-31-4-99.ap-northeast-1.compute.internal>
2023-07-18 15:01:22 +0000

    2

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,11 @@
+class HomesController < ApplicationController
+  def top
+  end
+
+  def about
+  end
+  
+  def new_guest
+    user = User.find_or_create_by(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+end
